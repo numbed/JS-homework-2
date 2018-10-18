32f0e78c5a3f028140c59617bfcfc787e4046534
Nikola Rashkov <rashkov.nikola@gmail.com>
2018-10-18 22:49:37 +0300

    Update 2.1.js

---
+++ 2.1.js
@@ -0,0 +1,28 @@
+function run(){
+	var alice = [];
+
+	for (var i=1; i<4; i++){
+		var a = prompt("Enter a"+i+":");
+		a = parseInt(a);
+		alice.push(a);
+
+	}
+	var bob = [];
+	for (var i=1; i<4; i++){
+		var b = prompt("Enter b"+i+":");
+		b = parseInt(b);
+		bob.push(b);
+	}
+	console.log(alice);
+	console.log(bob);
+
+	var alicePoints = 0, bobPoints = 0;
+	for (var x=0; x < alice.length; x++){
+			if (alice[x]>bob[x]) {
+				alicePoints++;
+			} else if (alice[x]<bob[x]) {
+				bobPoints++;
+			}
+	}
+	console.log("Alice: "+alicePoints+" Bob: "+ bobPoints);
+}
