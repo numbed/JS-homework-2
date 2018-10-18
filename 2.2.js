b6b40014fc3e09039894e548c6fa1714b5c3cc51
Nikola Rashkov <rashkov.nikola@gmail.com>
2018-10-18 23:27:19 +0300

    Update 2.2.js

---
+++ 2.2.js
@@ -0,0 +1,14 @@
+function run(){
+  var sentence = prompt("Enter camelCase sentence:");
+  var capitalNumber = 0;
+  for (i=o; i<sentence.length; i++) {
+    if (sentence[i] === sentence[i].toUpperCase();) {
+      capitalNumber++;
+    }
+  }
+
+
+  console.log(sentence);
+  console.log(capitalNumber);
+  console.log(newSentence);
+}
