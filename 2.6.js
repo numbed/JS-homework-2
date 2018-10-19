999c76c480b56e45230d7c7ccbdfc3a53d3bd11f
Nikola Rashkov <rashkov.nikola@gmail.com>
2018-10-19 16:28:03 +0300

    Update 2.6.js

--- 2.6.js
+++ 2.6.js
@@ -4,21 +4,18 @@ function run(){
 	var caesarShift = prompt("Enter Caesar shift factor:",3);
 	caesarShift = parseInt(caesarShift);
 	var encryptedText="";
-	var textCharCode="";
 	for (var n=0; n<text.length; n++){
 		 var textChar = text.charCodeAt(n);
-		 if (textChar => 65 && textChar <= 90) { 
+		 if (textChar >= 65 && textChar <= 90) { 
 			encryptedText += String.fromCharCode((textChar - 65 + caesarShift) % 26 +65);
-		} else if (textChar => 97 && textChar <= 122) {
+		} else if (textChar >= 97 && textChar <= 122) {
 			encryptedText += String.fromCharCode((textChar - 97 + caesarShift) % 26 + 97);
 		} else {
 			encryptedText += String.fromCharCode(textChar);
 		}
-		textCharCode += (textChar+" ");	
 	}
 	
 	console.log(text);
-	console.log(textCharCode);
 	console.log(encryptedText);
 
 	
