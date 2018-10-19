769bc45135c82325d2e75286fbd53dc98829b577
Nikola Rashkov <rashkov.nikola@gmail.com>
2018-10-19 10:08:07 +0300

    Create 2.3.js

---
+++ 2.3.js
@@ -0,0 +1,15 @@
+function run(){
+	var a = [];
+	var arraySum=0;
+	while (true) {
+		var x = prompt("Enter int number.\nCancel to stop.");
+		if (!x) break;
+		x = parseInt(x);
+		a.push(x);
+	}
+	console.log(a);
+	for (var i=0; i<a.length; i++) {
+		arraySum += a[i];
+	}
+	console.log(arraySum);
+}
