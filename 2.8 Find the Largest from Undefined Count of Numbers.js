ff2ac7c06325cfcbe41e8a5982ba084d3d4d3835
Nikola Rashkov <rashkov.nikola@gmail.com>
2018-10-23 14:53:42 +0300

    Create 2.8 Find the Largest from Undefined Count of Numbers.js

---
+++ 2.8 Find the Largest from Undefined Count of Numbers.js
@@ -0,0 +1,20 @@
+/*
+  Find the largest number; the input numbers might contain negatives.
+*/
+
+function run(){
+	var a = [];
+	var arraySum=0;
+	while (true) {
+		var x = prompt("Enter int number.\nCancel to stop.");
+		if (!x) break;
+		x = parseInt(x);
+		a.push(x);
+	}
+	console.log(a);
+	var aSorted = a.sort(function(a,b){return a-b});
+	console.log(aSorted);
+	console.log(aSorted.pop());
+
+}
+run();
