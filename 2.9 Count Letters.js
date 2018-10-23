ce237d2ef5a5f62f92b9153867d28c1236628326
Nikola Rashkov <rashkov.nikola@gmail.com>
2018-10-23 15:01:51 +0300

    Create 2.9 Count Letters.js

---
+++ 2.9 Count Letters.js
@@ -0,0 +1,7 @@
+/*
+  You will receive a single line from the standard input containing a word (or at least a set or characters). 
+  You need to print on the standard input how many times each letter is found in order of the letter appearance, 
+  in format {letter} -> {times}
+*/
+
+
